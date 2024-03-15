a9a12f6a9fc2c50326e16d65550f6359320eb623
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-03-16 03:20:57 +0530

    Created vite + react + tailwind starter pack

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1 className="">this is starter package</h1>
+    </div>
+  );
+};
+
+export default App;
