961fa8cf89df2d89cf3b7a4dfc46c84fa09f70e2
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-03-16 17:45:12 +0530

    Revert "created a mini project of React added tailwind"
    This reverts commit 1c2b0b9d876767e559415eaca58ab703b1ca6a95.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,5 @@
-import { useState } from "react";
-import Tours from "./components/Tours";
-import data from "./utils/data";
-
 const App = () => {
-  const [tours, setTours] = useState(data);
-
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id);
-    setTours(newTours);
-  };
-
-  if (tours.length === 0) {
-    return (
-      <div className="flex justify-center items-center flex-col w-screen h-screen">
-        <h2 className="text-3xl font-bold text-blue-800 border-b-4 mb-2 border-red-400">
-          No Tours left
-        </h2>
-        <button
-          onClick={() => setTours(data)}
-          className="bg-black text-white h-8 font-bold text-2xl w-[100px] rounded-md mb-40 hover:text-black  hover:bg-gray-200"
-        >
-          Refresh
-        </button>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <Tours tours={tours} removeTour={removeTour}></Tours>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
